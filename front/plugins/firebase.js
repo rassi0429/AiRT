1c7e4aafe84b1d2f0ba26e33c86afc94f9c2a71b
rassi0429 <taitokokoa+rassi@gmail.com>
2022-10-11 07:28:17 +0900

    feat: update firebase config

--- front/plugins/firebase.js
+++ front/plugins/firebase.js
@@ -5,12 +5,12 @@ import 'firebase/auth'
 // ** 本番環境
 if (!firebase.apps.length) {
   firebase.initializeApp({
-    apiKey: "AIzaSyDXykK8y4smDE8d19ozk3caN685vhT2vh8",
-    authDomain: "photo-neos-love.firebaseapp.com",
-    projectId: "photo-neos-love",
-    storageBucket: "photo-neos-love.appspot.com",
-    messagingSenderId: "871699453882",
-    appId: "1:871699453882:web:1eedd757d605b91e0a2f83"
+    apiKey: "AIzaSyBfKk3_OlUkYPwnTRlq0OS5BDHRMynUpmw",
+    authDomain: "airt-cc.firebaseapp.com",
+    projectId: "airt-cc",
+    storageBucket: "airt-cc.appspot.com",
+    messagingSenderId: "798657714288",
+    appId: "1:798657714288:web:343de6551beec834fb7cb5"
   })
 }
 
